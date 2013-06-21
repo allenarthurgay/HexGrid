8023576bfe3f8e4325b137fea7c9fb3aee231633
Allen Gay <allenathurgay@gmail.com>
2013-06-21 09:24:35 -0700

    a little bit of stuff

--- hexgrid.js
+++ hexgrid.js
@@ -4,10 +4,10 @@ function Hex(x, y, radius) {
 
 	this.x = this.q;
 	this.z = this.r
-	this.y = -this.x-this.z;
+	this.y = -this.x - this.z;
 
 	this.size = radius;
-    this.color=[1,1,1,1];
+	this.color = [1, 1, 1, 1];
 	this.height = function() {return this.size * 2;}
 	this.width = function() {return 1.7320508 * this.size;} //﻿1.7320508 is ~ Math.sqrt(3)
 	this.equals = function(rHex) {
@@ -25,7 +25,7 @@ function HexRenderEngine(canvas, use3dRendering) {
 	var ctx_2D = undefined;
 	var drawHex = undefined;
 	var pointUpHexVertexBuffer;
-    var hexOutlineVertexBuffer;
+	var hexOutlineVertexBuffer;
 	var fieldOfVision = 90;
 	var mvMatrix = mat4.create(); //modelview
 	var pMatrix = mat4.create();  //perspective
@@ -84,7 +84,7 @@ function HexRenderEngine(canvas, use3dRendering) {
 		shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
 		shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
 
-        shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+		shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
 		shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 	}
 
@@ -111,10 +111,10 @@ function HexRenderEngine(canvas, use3dRendering) {
 		 0,-0.5
 		 */
 		pointUpHexVertexBuffer = gl.createBuffer();
-        hexOutlineVertexBuffer = gl.createBuffer();
+		hexOutlineVertexBuffer = gl.createBuffer();
 
-        var hexVerts = [0.0,0.0,0.0];
-        var outlineVerts = [];
+		var hexVerts = [0.0, 0.0, 0.0];
+		var outlineVerts = [];
 
 		for (var i = 0; i <= 6; i++) {
 			var angle = 2 * Math.PI / 6 * (0.5 - i);
@@ -122,118 +122,122 @@ function HexRenderEngine(canvas, use3dRendering) {
 			var x_i = 0.5 * Math.cos(angle);
 			var y_i = 0.5 * Math.sin(angle);
 
-            hexVerts.push(x_i);
-            hexVerts.push(y_i);
-            hexVerts.push(0.0);
-            outlineVerts.push(x_i);
-            outlineVerts.push(y_i);
-            outlineVerts.push(0.01);
+			hexVerts.push(x_i);
+			hexVerts.push(y_i);
+			hexVerts.push(0.0);
+			outlineVerts.push(x_i);
+			outlineVerts.push(y_i);
+			outlineVerts.push(0.01);
 		}
 
 		// re add first point to complete fan
-        hexVerts.push(0.5 * Math.cos(2 * Math.PI / 3));
-        hexVerts.push(0.5 * Math.sin(2 * Math.PI / 3));
-        hexVerts.push(0.0);
+		hexVerts.push(0.5 * Math.cos(2 * Math.PI / 3));
+		hexVerts.push(0.5 * Math.sin(2 * Math.PI / 3));
+		hexVerts.push(0.0);
 
-        //re add first point to complete outline
-        outlineVerts.push(outlineVerts[0]);
-        outlineVerts.push(outlineVerts[1]);
-        outlineVerts.push(0.0);
+		//re add first point to complete outline
+		outlineVerts.push(outlineVerts[0]);
+		outlineVerts.push(outlineVerts[1]);
+		outlineVerts.push(0.0);
 
 		gl.bindBuffer(gl.ARRAY_BUFFER, pointUpHexVertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(hexVerts), gl.STATIC_DRAW);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(hexVerts), gl.STATIC_DRAW);
 		gl.bindBuffer(gl.ARRAY_BUFFER, null);
 		pointUpHexVertexBuffer.itemSize = 3;
 		pointUpHexVertexBuffer.numItems = 8;
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, hexOutlineVertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(outlineVerts), gl.STATIC_DRAW);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-        hexOutlineVertexBuffer.itemSize = 3;
-        hexOutlineVertexBuffer.numItems = 7;
+		gl.bindBuffer(gl.ARRAY_BUFFER, hexOutlineVertexBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(outlineVerts), gl.STATIC_DRAW);
+		gl.bindBuffer(gl.ARRAY_BUFFER, null);
+		hexOutlineVertexBuffer.itemSize = 3;
+		hexOutlineVertexBuffer.numItems = 7;
 	}
 
-    function unprojectPoint(sx, sy) {
-        var cameraMatrix = mat4.create();
-        var projectionMatrix = mat4.create();
-        var inverseMatrix = mat4.create();
-        var pvMat = mat4.create();
+	function unprojectPoint(sx, sy) {
+		var cameraMatrix = mat4.create();
+		var projectionMatrix = mat4.create();
+		var inverseMatrix = mat4.create();
+		var pvMat = mat4.create();
 
-        mat4.identity(inverseMatrix);
-        setViewMatrix(cameraMatrix);
-        setProjectionMatrix(projectionMatrix);
-        mat4.multiply(projectionMatrix, cameraMatrix, pvMat);
-        mat4.inverse(pvMat, inverseMatrix);
-        var inNear = [];
-        inNear[0] = (sx - gl.viewportX)/gl.viewportWidth*2.0 - 1.0; // normalize window X coord to -1 to 1
-        inNear[1] = (sy - gl.viewportY)/gl.viewportHeight*2.0 - 1.0; // normalize window Y coord to -1 to 1
-        inNear[2] = -1.0; // 2.0 * sz - 1.0 but optimized for near plane click
-        inNear[3] = 1.0; // 1 = point, 0 = vector
+		mat4.identity(inverseMatrix);
+		setViewMatrix(cameraMatrix);
+		setProjectionMatrix(projectionMatrix);
+		mat4.multiply(projectionMatrix, cameraMatrix, pvMat);
+		mat4.inverse(pvMat, inverseMatrix);
+		var inNear = [];
+		inNear[0] = (sx - gl.viewportX) / gl.viewportWidth * 2.0 - 1.0; // normalize window X coord to -1 to 1
+		inNear[1] = (sy - gl.viewportY) / gl.viewportHeight * 2.0 - 1.0; // normalize window Y coord to -1 to 1
+		inNear[2] = -1.0; // 2.0 * sz - 1.0 but optimized for near plane click
+		inNear[3] = 1.0; // 1 = point, 0 = vector
 
-        var inFar = [];
-        inFar[0] = (sx - gl.viewportX)/gl.viewportWidth*2.0 - 1.0; // normalize window X coord to -1 to 1
-        inFar[1] = (sy - gl.viewportY)/gl.viewportHeight*2.0 - 1.0; // normalize window Y coord to -1 to 1
-        inFar[2] = 1.0; // 2.0 * sz - 1.0 but optimized for far plane click
-        inFar[3] = 1.0; // 1 = point, 0 = vector
+		var inFar = [];
+		inFar[0] = (sx - gl.viewportX) / gl.viewportWidth * 2.0 - 1.0; // normalize window X coord to -1 to 1
+		inFar[1] = (sy - gl.viewportY) / gl.viewportHeight * 2.0 - 1.0; // normalize window Y coord to -1 to 1
+		inFar[2] = 1.0; // 2.0 * sz - 1.0 but optimized for far plane click
+		inFar[3] = 1.0; // 1 = point, 0 = vector
 
-        var near = [0,0,0,0];
-        var far = [0,0,0,0];
+		var near = [0, 0, 0, 0];
+		var far = [0, 0, 0, 0];
 
-        mat4.multiply(inverseMatrix, inNear, near);
+		mat4.multiply(inverseMatrix, inNear, near);
 
-        if(near[3] == 0.0) {
-            console.log("unproject point resulted in 0 for w coordinate. Does this ever happen?");
-            return null;
-        }
+		if (near[3] == 0.0) {
+			console.log("unproject point resulted in 0 for w coordinate. Does this ever happen?");
+			return null;
+		}
 
-        //normalize output
-        near[3] = 1.0/near[3];
-        near[0] = near[0]*near[3];
-        near[1] = near[1]*near[3];
-        near[2] = near[2]*near[3];
+		//normalize output
+		near[3] = 1.0 / near[3];
+		near[0] = near[0] * near[3];
+		near[1] = near[1] * near[3];
+		near[2] = near[2] * near[3];
 
 
-        mat4.multiply(inverseMatrix, inFar, far);
+		mat4.multiply(inverseMatrix, inFar, far);
 
-        if(far[3] == 0.0) {
-            console.log("unproject point resulted in 0 for w coordinate. Does this ever happen?");
-            return null;
-        }
+		if (far[3] == 0.0) {
+			console.log("unproject point resulted in 0 for w coordinate. Does this ever happen?");
+			return null;
+		}
 
-        //normalize output
-        far[3] = 1.0/far[3];
-        far[0] = far[0]*far[3];
-        far[1] = far[1]*far[3];
-        far[2] = far[2]*far[3];
+		//normalize output
+		far[3] = 1.0 / far[3];
+		far[0] = far[0] * far[3];
+		far[1] = far[1] * far[3];
+		far[2] = far[2] * far[3];
 
-        return [[near[0], near[1], near[2]],[far[0], far[1], far[2]]];
-    }
+		return [
+			[near[0], near[1], near[2]],
+			[far[0], far[1], far[2]]
+		];
+	}
 
-    function setProjectionMatrix(m) {
-        mat4.identity(m);
-        mat4.perspective(fieldOfVision/2.0, gl.viewportWidth / gl.viewportHeight, 0.1, 1000.0, m);
-    }
+	function setProjectionMatrix(m) {
+		mat4.identity(m);
+		mat4.perspective(fieldOfVision / 2.0, gl.viewportWidth / gl.viewportHeight, 0.1, 1000.0, m);
+	}
 
-    function setViewMatrix(m) {
-        mat4.identity(m);
-        mat4.translate(m, [-cam_x,-cam_y,-cam_z]);
-        mat4.rotate(m,degree*Math.PI/180.0,[1,0,0],m);
-    }
+	function setViewMatrix(m) {
+		mat4.identity(m);
+		mat4.translate(m, [-cam_x, -cam_y, -cam_z]);
+		mat4.rotate(m, degree * Math.PI / 180.0, [1, 0, 0], m);
+	}
 
-    function setModelViewMatrix(x,y,r) {
-        setViewMatrix(mvMatrix);
-        mat4.translate(mvMatrix, [x,y,0]);
-        mat4.scale(mvMatrix,[r*2,r*2,1]);
+	function setModelViewMatrix(x, y, r) {
+		setViewMatrix(mvMatrix);
+		mat4.translate(mvMatrix, [x, y, 0]);
+		mat4.scale(mvMatrix, [r * 2, r * 2, 1]);
 
-    }
-    var drawHex3D = function(x,y,r,fillColor) {
+	}
 
-        setModelViewMatrix(x,y,r);
+	var drawHex3D = function(x, y, r, fillColor) {
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, pointUpHexVertexBuffer);
+		setModelViewMatrix(x, y, r);
 
-        //set color
-        gl.vertexAttrib4fv(shaderProgram.vertexColorAttribute, new Float32Array(fillColor));
+		gl.bindBuffer(gl.ARRAY_BUFFER, pointUpHexVertexBuffer);
+
+		//set color
+		gl.vertexAttrib4fv(shaderProgram.vertexColorAttribute, new Float32Array(fillColor));
 
 
 		setMatrixUniforms();
@@ -241,29 +245,29 @@ function HexRenderEngine(canvas, use3dRendering) {
 		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, pointUpHexVertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 		gl.drawArrays(gl.TRIANGLE_FAN, 0, pointUpHexVertexBuffer.numItems);
-        gl.bindBuffer(gl.ARRAY_BUFFER,null);
+		gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-        //outline =========================
-        gl.bindBuffer(gl.ARRAY_BUFFER, hexOutlineVertexBuffer);
-        //set color
-        gl.vertexAttrib4fv(shaderProgram.vertexColorAttribute, new Float32Array([0,0,1,1]));
-        setMatrixUniforms();
-        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, hexOutlineVertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-        gl.drawArrays(gl.LINE_STRIP,0,hexOutlineVertexBuffer.numItems);
+		//outline =========================
+		gl.bindBuffer(gl.ARRAY_BUFFER, hexOutlineVertexBuffer);
+		//set color
+		gl.vertexAttrib4fv(shaderProgram.vertexColorAttribute, new Float32Array([0, 0, 1, 1]));
+		setMatrixUniforms();
+		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, hexOutlineVertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+		gl.drawArrays(gl.LINE_STRIP, 0, hexOutlineVertexBuffer.numItems);
 		gl.bindBuffer(gl.ARRAY_BUFFER, null);
 	}
 
 	var drawHex2D = function(x, y, r, fillColor) {
 		var g = ctx_2D.createRadialGradient(x, y, 0, x, y, 0.8 * r);
-        var color = '#';
-        for(var c = 0; c < 3; ++c) {
-            var val = (255 * fillColor[c]).toString(16);
-            var pad = "00";
-            color = color + pad.substr(0,pad.length - val.length) + val;
-        }
+		var color = '#';
+		for (var c = 0; c < 3; ++c) {
+			var val = (255 * fillColor[c]).toString(16);
+			var pad = "00";
+			color = color + pad.substr(0, pad.length - val.length) + val;
+		}
 		g.addColorStop(0, '#fff');
-        g.addColorStop(1,color);
+		g.addColorStop(1, color);
 		ctx_2D.fillStyle = g;
 		ctx_2D.beginPath();
 		ctx_2D.lineWidth = 4;
@@ -292,20 +296,20 @@ function HexRenderEngine(canvas, use3dRendering) {
 			var hex = hexes[i];
 			//var x = hex.width() * (hex.q + hex.r/2)
 			//var y = hex.height() * hex.r
-		//	console.log(hex.q, )
+			//	console.log(hex.q, )
 			//var x = hex.q % 2 == 0 ? 0 : hex.width() / 2.0;
-			var x= hex.width() * (hex.q + hex.r / 2);
-			var y =  hex.height() * hex.r * 3 / 4.0;
-		//	x += hex.width() * hex.q;
+			var x = hex.width() * (hex.q + hex.r / 2);
+			var y = hex.height() * hex.r * 3 / 4.0;
+			//	x += hex.width() * hex.q;
 
-            drawHex(x,y,hex.size,hex.color);
-        }
+			drawHex(x, y, hex.size, hex.color);
+		}
 
 		postRender();
 	}
 
 	var preRender3D = function() {
-        setProjectionMatrix(pMatrix);
+		setProjectionMatrix(pMatrix);
 	}
 
 	var preRender2D = function() {}
@@ -324,9 +328,9 @@ function HexRenderEngine(canvas, use3dRendering) {
 
 	var clear3D = function() {
 		gl.clearColor(0, 0, 0, 1.0);
-        gl.viewportX = 0;
-        gl.viewportY = 0;
-        gl.viewport(gl.viewportX,gl.viewportY,gl.viewportWidth,gl.viewportHeight);
+		gl.viewportX = 0;
+		gl.viewportY = 0;
+		gl.viewport(gl.viewportX, gl.viewportY, gl.viewportWidth, gl.viewportHeight);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	}
 
@@ -367,39 +371,39 @@ function HexRenderEngine(canvas, use3dRendering) {
 		gl.depthFunc(gl.LEQUAL);
 	}
 
-    function intersectLineWithXYPlane(far, near) {
-        var vector = [far[0]-near[0],far[1]-near[1],far[2]-near[2]];
-        var t = -near[2] / vector[2];
-        return [near[0] + t * vector[0], near[1] + t * vector[1]];
-    }
+	function intersectLineWithXYPlane(far, near) {
+		var vector = [far[0] - near[0], far[1] - near[1], far[2] - near[2]];
+		var t = -near[2] / vector[2];
+		return [near[0] + t * vector[0], near[1] + t * vector[1]];
+	}
 
-    this.projectScreenCoordToXYPlane = function(screenX, screenY){
-        screenY = gl.viewportHeight - screenY;// canvas starts 0,top to H,bottom -- gl is opposite
-        var nearFarPoints = unprojectPoint(screenX,screenY);
-        return intersectLineWithXYPlane(nearFarPoints[0], nearFarPoints[1]);
-    }
+	this.projectScreenCoordToXYPlane = function(screenX, screenY) {
+		screenY = gl.viewportHeight - screenY;// canvas starts 0,top to H,bottom -- gl is opposite
+		var nearFarPoints = unprojectPoint(screenX, screenY);
+		return intersectLineWithXYPlane(nearFarPoints[0], nearFarPoints[1]);
+	}
 
-    if(!use3D || !supportsWebGL()) {
-        ctx_2D = canvas.getContext('2d');
-        use3D = false;
-        this.clearCanvas = clear2D;
-        drawHex = drawHex2D;
-        preRender = preRender2D;
-        postRender = postRender2D;
-    }
-    else {
-        initGl();
-        use3D = true;
-        this.clearCanvas = clear3D;
-        drawHex = drawHex3D;
-        preRender = preRender3D;
-        postRender = postRender3D;
-    }
+	if (!use3D || !supportsWebGL()) {
+		ctx_2D = canvas.getContext('2d');
+		use3D = false;
+		this.clearCanvas = clear2D;
+		drawHex = drawHex2D;
+		preRender = preRender2D;
+		postRender = postRender2D;
+	}
+	else {
+		initGl();
+		use3D = true;
+		this.clearCanvas = clear3D;
+		drawHex = drawHex3D;
+		preRender = preRender3D;
+		postRender = postRender3D;
+	}
 
 }
 
 function HexGrid(canvas, use3D) {
-    var hexSize = 1.5;
+	var hexSize = 1.5;
 	var hexRenderList = [];
 	var hexLookup = {};
 	var map = null;
@@ -440,13 +444,9 @@ function HexGrid(canvas, use3D) {
 		renderer.clearCanvas();
 	}
 
-	this.add = function(x, y) {
-		hexRenderList.push(new Hex(x, y, hexSize));
-	}
-
-	function pixelToHex(x, y){
-		var q = ((1/3*Math.sqrt(3) * (x) - 1/3 * y)) / hexSize;
-		var r = (2/3 * y) / hexSize;
+	function pixelToHex(x, y) {
+		var q = ((1 / 3 * Math.sqrt(3) * (x) - 1 / 3 * y)) / hexSize;
+		var r = (2 / 3 * y) / hexSize;
 		var ret = {
 			q: Math.round(q),
 			r: Math.round(r)
@@ -454,15 +454,15 @@ function HexGrid(canvas, use3D) {
 		return ret;
 	}
 
-	this.findByPixel = function(x, y){
-        if(use3D) {
-            var point = renderer.projectScreenCoordToXYPlane(x,y);
-            x = point[0];
-            y = point[1];
-            console.log('projected point: ', x, y);
-        }
+	this.findByPixel = function(x, y) {
+		if (use3D) {
+			var point = renderer.projectScreenCoordToXYPlane(x, y);
+			x = point[0];
+			y = point[1];
+			console.log('projected point: ', x, y);
+		}
 		var hexToFind = pixelToHex(x, y);
-        console.log('hex coords: ', hexToFind.q, hexToFind.r);
+		console.log('hex coords: ', hexToFind.q, hexToFind.r);
 		return this.find(hexToFind.q, hexToFind.r);
 	};
 
@@ -505,29 +505,29 @@ function HexGrid(canvas, use3D) {
 		this.draw();
 	}
 
-	this.setHexSize = function(newSize) {
-		hexSize = newSize;
-		this.applyToAllHexes(function(hex) {
-			hex.size = newSize;
-		});
+	this.findNeighbors = function(hex) {
+
+		var neighborDeltas = [
+			[+1, 0],
+			[+1, -1],
+			[ 0, -1],
+			[-1, 0],
+			[-1, +1],
+			[ 0, +1]
+		];
+
+		var neighbors = [];
+		for (var i = 0; i < neighborDeltas.length; ++i) {
+			var n = this.find(hex.q + neighborDeltas[i][0], hex.r + neighborDeltas[i][1]);
+			if (n) {
+				neighbors.push(n);
+			}
+		}
+		return neighbors;
 	}
-    this.findNeighbors = function(hex) {
-        var neighborDeltas =  [
-            [+1,  0],  [+1, -1],  [ 0, -1],
-            [-1,  0],  [-1, +1],  [ 0, +1]
-        ];
-        var neighbors = [];
-        for(var i = 0; i < neighborDeltas.length; ++i) {
-            var n = this.find(hex.q + neighborDeltas[i][0], hex.r + neighborDeltas[i][1]);
-            if(n) {
-                neighbors.push(n);
-            }
-        }
-        return neighbors;
-    }
 
 	this.update = function() {}
 
-    this.pickHex = function(x,y) {
-    }
+	this.pickHex = function(x, y) {
+	}
 }
